bca4e52805379387e4d8df7682fa9765914d4758
Aaron Keane <ak30195@hotmail.com>
2025-09-07 18:21:15 +0100

    Minor change to loading views.

--- src/components/DataFetcher.jsx
+++ src/components/DataFetcher.jsx
@@ -2,8 +2,8 @@ import Loader from "./Loader";
 import ErrorMessage from "./ErrorMessage";
 
 
-export default function DataFetcher({ loading, error, children, loadingText = "Loading..." }) {
-  if (loading) return <Loader text={loadingText} />;
+export default function DataFetcher({ loading, error, children}) {
+  if (loading) return <Loader />;
   if (error) return <ErrorMessage message={error.message} />;
 
   return <>{children}</>;
